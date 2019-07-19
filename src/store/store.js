f8530db9886bff35c45ebefa7841ffa83e634f94
endorphin82 <endorphin82@gmail.com>
2019-07-19 15:55:29 +0300

    02-add_redux_tools_store_bootstrap

---
+++ src/store/store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import reducer from "../reducer";
+import { routerMiddleware } from "connected-react-router";
+import { history } from "../history";
+
+
+const routerHistoryMiddleware = routerMiddleware(history);
+
+const composeEnhancers =
+  typeof window === "object" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(routerHistoryMiddleware)
+);
+
+const store = createStore(reducer, {}, enhancer);
+
+export default store;
