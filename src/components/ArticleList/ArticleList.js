79d9fba4b4af96600ba2ffced12c062699141bf2
endorphin82 <endorphin82@gmail.com>
2019-07-20 05:05:40 +0300

    08-add_layout

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -3,7 +3,7 @@ import { loadAllArticles } from "../../AC";
 import { connect } from "react-redux";
 import Loader from "../Loader";
 import { NavLink } from "react-router-dom";
-import { articlesCountGetterSelector, ArticleSelector, commentsCountGetterSelector } from "../../selectors";
+import { ArticleSelector } from "../../selectors";
 import { Button, Table } from "reactstrap";
 
 class ArticleList extends Component {
@@ -14,7 +14,7 @@ class ArticleList extends Component {
   }
 
   render() {
-    const { articles, loading, countComments, countArticles } = this.props;
+    const { articles, loading } = this.props;
     if (!loading) return <Loader/>;
     console.log(articles);
     const articleElements = articles.map((article) => <tr key={article.postId}>
@@ -36,7 +36,6 @@ class ArticleList extends Component {
                   d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-4 6V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1z"/>
               </svg>
             </Button>
-
             <Button color="danger">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M0 0h24v24H0z" fill="none"/>
@@ -44,30 +43,26 @@ class ArticleList extends Component {
                   d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
               </svg>
             </Button>
-
           </div>
-
-
         </td>
       </tr>
     );
 
     return (
       <>
-        {countComments} comments in {countArticles} articles
         <Table striped bordered hover>
-        <thead>
-        <tr>
-          <th>Article Title</th>
-          <th>Content</th>
-          <th>Comments</th>
-          <th>Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-        {articleElements}
-        </tbody>
-      </Table>
+          <thead>
+          <tr>
+            <th>Article Title</th>
+            <th>Content</th>
+            <th>Comments</th>
+            <th>Actions</th>
+          </tr>
+          </thead>
+          <tbody>
+          {articleElements}
+          </tbody>
+        </Table>
       </>
 
     );
@@ -77,8 +72,6 @@ class ArticleList extends Component {
 export default connect((state) => {
   return {
     articles: ArticleSelector(state),
-    loading: state.articles.loading,
-    countArticles: articlesCountGetterSelector(state),
-    countComments: commentsCountGetterSelector(state)
+    loading: state.articles.loading
   };
 }, { loadAllArticles })(ArticleList);
--- src/components/routes/MainArticles.js
+++ src/components/routes/MainArticles.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 import ArticleList from "../ArticleList/ArticleList";
+import {Row, Col, Container} from "reactstrap";
+import ArticlesCount from "../ArticlesCount/ArticlesCount";
+import CommentsCount from "../CommentsCount/CommentsCount";
 
 
 class MainArticles extends Component {
   render() {
     return (
-      <>
-        <ArticleList />
-      </>
+      <Container>
+        <Row>
+          <Col>
+          <h2>Articles</h2>
+            <ArticleList/>
+          </Col>
+        </Row>
+        <Row className="mb-5">
+          <Col><ArticlesCount/></Col>
+          <Col><CommentsCount/></Col>
+        </Row>
+      </Container>
     );
   }
 }
