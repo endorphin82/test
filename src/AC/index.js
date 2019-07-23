53d7b452dc516dfc82177cf9dbc675f73645e6f3
endorphin82 <endorphin82@gmail.com>
2019-07-23 14:17:08 +0300

    10-refactor_cards_counters

--- src/AC/index.js
+++ src/AC/index.js
@@ -7,3 +7,25 @@ export function loadAllArticles() {
     callAPI: `http://localhost:3004/posts`
   };
 };
+
+
+// width THUNK
+// import { FAILURE, LOAD_ALL_ARTICLES, START, SUCCESS } from "../constants";
+// import axios  from "axios";
+//
+// export const loadAllArticles = () => dispatch => {
+//   dispatch({
+//     type: LOAD_ALL_ARTICLES + START
+//   });
+//   return axios
+//     .get("http://localhost:3004/posts")
+//     .then(response => response.data)
+//     .then(response => dispatch({
+//       type: LOAD_ALL_ARTICLES + SUCCESS,
+//       response
+//     }))
+//     .catch(errors => dispatch({
+//       type: LOAD_ALL_ARTICLES + FAILURE,
+//       payload: { errors }
+//     }));
+// };
