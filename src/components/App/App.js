d0988e735e8bb14d946281a00c98f64fb5a4edb5
endorphin82 <endorphin82@gmail.com>
2019-08-06 08:31:04 +0300

    14.1-add_material_fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,37 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import { ConnectedRouter } from "connected-react-router";
-import { connect } from "react-redux";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-import theme from 'components/theme.js'
-
-import { loadAllArticles } from "AC";
+import theme from "components/theme.js";
 import { history } from "history.js";
-
-import "./App.css";
-// import MainArticles from "components/routes/MainArticles";
 import Articles from "../Articles/Articles";
 
-class App extends Component {
-  render() {
-    return (
-      <ConnectedRouter history={history}>
-        <MuiThemeProvider theme={theme}>
-          <Articles/>
-        </MuiThemeProvider>
-        {/*<MainArticles/>*/}
-      </ConnectedRouter>
-    );
-  }
-
-  handleClick = e => {
-    e.preventDefault();
-    console.log("click", e);
-    this.props.loadAllArticles();
-  };
+function App() {
+  return (
+    <ConnectedRouter history={history}>
+      <MuiThemeProvider theme={theme}>
+        <Articles/>
+      </MuiThemeProvider>
+    </ConnectedRouter>
+  );
 }
 
-const mapStateToProps = state => ({
-  posts: state.posts
-});
 
-export default connect(mapStateToProps, { loadAllArticles })(App);
+export default App;
