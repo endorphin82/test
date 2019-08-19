7dafe75602f7776beea0ee14c6d5695e7a8a8335
endorphin82 <endorphin82@gmail.com>
2019-08-19 14:54:59 +0300

    Updates

---
+++ precache-manifest.9307e509cdcf7d34c6dd5e0a00580361.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1faf58d9b3a811d16d72dfcd0ff050ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "903e9b9b780c284214b7",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fd967c29974bbdf8f527",
+    "url": "./static/js/2.0b09a62b.chunk.js"
+  },
+  {
+    "revision": "903e9b9b780c284214b7",
+    "url": "./static/js/main.ba0ba900.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
